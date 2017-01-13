3c48ddc31bff1e68724ef9f0a7b8cce73e578478
Heelo Mangola <heelomangola@gmail.com>
2017-01-13 21:04:44 +0100

    deplacement et collisions

---
+++ scripts/BordureGauche.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BordureGauche : MonoBehaviour {
+	Vector3 hg;
+	Vector3 bg;
+	public float width = 0.5F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Camera.main.transform.position.z;
+		hg = Camera.main.ViewportToWorldPoint(new Vector3(0,1,-dist));
+		bg = Camera.main.ViewportToWorldPoint(new Vector3(0,0,-dist));
+		transform.position = bg;
+		Vector3 dimensions = bg - hg;
+		Vector3 position = dimensions;
+		position.Scale (new Vector3 (0.5F, 0.5F, 0.5F));
+		transform.Translate (position);
+		dimensions.x = width;
+		transform.localScale = dimensions;
+	}
+}
---
+++ scripts/bordureHaute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bordureHaute : MonoBehaviour {
+	Vector3 hg;
+	Vector3 hd;
+	public float width = 0.5F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Camera.main.transform.position.z;
+		hd = Camera.main.ViewportToWorldPoint(new Vector3(1,1,-dist));
+		hg = Camera.main.ViewportToWorldPoint(new Vector3(0,1,-dist));
+		transform.position = hg;
+		Vector3 dimensions = hg - hd;
+		Vector3 position = dimensions;
+		position.Scale (new Vector3 (0.5F, 0.5F, 0.5F));
+		transform.Translate (position);
+		dimensions.y = width;
+		transform.localScale = dimensions;
+	}
+}
