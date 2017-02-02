e0d8dff1aadeb928975666d2ecf9c711b1abda55
razeltif <razeltif@gmail.com>
2017-02-02 08:10:36 +0100

    script deplacement manette (en cours)

---
+++ scripts/deplacement/DeplacementManetteSCript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementManetteSCript : MonoBehaviour {
+
+	void Awake(){
+		string[] names = Input.GetJoystickNames();
+		Debug.Log ("Connected Joysticks ");
+		for (int i = 0; i < names.Length; i++) {
+			Debug.Log("Joystick" + (i+1) + " = " + names[i]);
+		
+		}
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DebugJoystickButtonPressed ();
+	}
+
+
+	void DebugJoystickButtonPressed(){
+		int joyNum = 1;
+		int buttonNum = 0;
+		int keyCode = 350;
+
+		for (int i = 0; i < 60; i++) {
+			if (Input.GetKeyDown ("" + keyCode + i))
+				Debug.Log ("Pressed! Joystick " + joyNum + " Button " + buttonNum + " @ " + Time.time);
+
+			buttonNum++;
+			if(buttonNum == 20){
+				buttonNum = 0;
+				joyNum++;
+			}
+		}
+	}
+}
