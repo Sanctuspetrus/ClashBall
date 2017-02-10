240832927dd3250ee162a2b702dafa863eaa51f1
razeltif <razeltif@gmail.com>
2017-02-10 23:08:38 +0100

    DANS TON CUL PUTAIN

---
+++ scripts/Helpers/SoundEffectHelper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectHelper : MonoBehaviour {
+
+	public static SoundEffectHelper Instance;
+
+	public AudioClip explosionSound;
+	public AudioClip maguichSound;
+
+	void Awake(){
+
+		//Singleton
+		if (Instance != null) {
+			Debug.LogError ("Multiple instances of SoundEffectsHelper!");
+		}
+		Instance = this;
+	}
+
+	public void MakeExplosionSound(Vector3 pos)
+	{
+		MakeSound(explosionSound, pos);
+	}
+
+	public void MakeMaguichSound(Vector3 pos)
+	{
+		MakeSound(maguichSound, pos);
+	}
+
+
+
+	private void MakeSound(AudioClip originalClip, Vector3 pos){
+		AudioSource.PlayClipAtPoint (originalClip, pos);
+	}
+
+
+}
---
+++ scripts/PositionColision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionColision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//On vérifie si la colision est détectée à l'arriere du joueur
+	void OnCollisionEnter2D(Collision2D col){
+		foreach (ContactPoint2D contact in col.contacts) {
+			Debug.Log (contact.point);
+
+			Vector2 direction = new Vector2 ();
+			direction.x = transform.position.x - contact.point.x; 
+			direction.y = transform.position.y - contact.point.y; 
+
+			Vector2 angle = new Vector2(Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad), Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad));
+			float angle2 = Vector2.Angle (angle, direction);
+
+			Vector3 cross = Vector3.Cross(angle, direction);
+			if (cross.z > 0)
+				angle2 = 360-angle2;
+
+			//Debug.Log ("angle : " + angle2 + " || rotation : " + direction + "  || tranfo : " + transform.eulerAngles.z);
+			if (angle2 > 180) {
+				Debug.Log ("zertghyjuk");
+				SoundEffectHelper.Instance.MakeMaguichSound (transform.position);
+			}
+
+
+
+		}
+
+	}
+}
