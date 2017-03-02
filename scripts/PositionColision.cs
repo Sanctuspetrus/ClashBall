49deaa9747aa050a8bbcb050ff7673641386f105
razeltif <razeltif@gmail.com>
2017-03-02 14:08:09 +0100

    animation done

--- scripts/PositionColision.cs
+++ scripts/PositionColision.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PositionColision : MonoBehaviour {
 
 	public int angleVulnerable = 90;
+	public int score = 0;
+
+	public Text scoreText;
 
 	// Use this for initialization
 	void Start () {
@@ -38,6 +42,9 @@ public class PositionColision : MonoBehaviour {
 
 				if (angle2 > (180 + variateur) && angle2 < (180 + variateur + angleVulnerable)) {
 					SoundEffectHelper.Instance.MakeMaguichSound (transform.position);
+					score++;
+					scoreText.text = score.ToString ();
+
 				}
 					
 			}
